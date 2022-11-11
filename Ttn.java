b3ee5bc823ec2d01b795a66f701bf7423701d39c
sarika2011 <114053434+sarika2011@users.noreply.github.com>
2022-11-11 11:55:54 +0530

    Create Ttn.java

---
+++ Ttn.java
@@ -0,0 +1,13 @@
+// service ke ander first interface
+
+package com.Bootcamp.core.services;
+
+public interface Ttn {
+
+	public String getName();
+	public int getNoe();
+	public String getDesc();
+	public String getLoc();
+	public String getServ();
+}
+Footer
